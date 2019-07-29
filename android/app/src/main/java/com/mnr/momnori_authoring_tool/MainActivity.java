02d56d728fd024c4144f8deb551a1f08f1422909
Wonkun Kim <wongni@gmail.com>
2019-07-29 18:11:20 -0500

    First commit

---
+++ android/app/src/main/java/com/mnr/momnori_authoring_tool/MainActivity.java
@@ -0,0 +1,13 @@
+package com.mnr.momnori_authoring_tool;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
